6171ed07efb8ae97993974b07fc8f0c9a40e91c2
forsuccess <474544358@qq.com>
2017-07-01 19:55:15 +0800

    '看看bundel.js'

--- app/index.js
+++ app/index.js
@@ -1,12 +1,1 @@
-import _ from 'lodash';
-import j from 'jquery'
-function component () {
-  var element = j('<div></div>');
-
-  /* lodash is required for the next line to work */
-  element.html(_.join(['Hello','webpack'], ' '))
-
-  return element.get(0);
-}
-
-document.body.appendChild(component());
+console.log(1);
